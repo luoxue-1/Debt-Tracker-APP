00fed07ea3156aaae9623e9f5fa1749061264835
luoxue-1 <heqing817@foxmail.com>
2026-02-19 13:48:29 +0800

    “111”

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\Asoftware\\flutter_windows_3.41.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\欠款记账" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\Asoftware\\flutter_windows_3.41.1-stable\\flutter"
+  "PROJECT_DIR=E:\\欠款记账"
+  "FLUTTER_ROOT=E:\\Asoftware\\flutter_windows_3.41.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\欠款记账\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\欠款记账"
+  "FLUTTER_TARGET=E:\\欠款记账\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NTgyYTBlN2M1NQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MzQ1MmQ3MzViZA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4w"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\欠款记账\\.dart_tool\\package_config.json"
+)
